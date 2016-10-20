a3a754722dedf134794ab22829b8b21f38fcdb45
pegartillo95 <pegarc03@ucm.es>
2016-10-20 15:47:11 +0200

    Minimum changes

--- ex1.m
+++ ex1.m
@@ -11,7 +11,6 @@ plotData(X, y);
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
-
 %-------- Running Gradient descent------
 fprintf('Running Gradient Descent ...\n')
 
@@ -23,10 +22,17 @@ theta = zeros(2, 1); % initialize fitting parameters
 	alpha = 0.01;
 
 % compute and display initial cost
-costFunction(X, y, theta)
+costFunction(X, y, theta);
 
 % run gradient descent
 theta = gradientDescent(X, y, theta, alpha, iterations);
 
 fprintf('Theta found by gradient descent: ');
 fprintf('%f %f \n', theta(1), theta(2));
+
+
+% Plot the linear fit
+hold on; % keep previous plot visible
+plot(X(:,2), X*theta, '-')
+legend('Training data', 'Linear regression')
+hold off % don't overlay any more plots on this figure

b5efc86c3a09a67e159cdb6e27d4954e0e88d771
pegartillo95 <pegarc03@ucm.es>
2016-10-20 15:46:34 +0200

    Minimum changes

--- costFunction.m
+++ costFunction.m
@@ -1,10 +1,10 @@
-function J = costFunction(x,y,theta)
+function J = costFunction(X,y,theta)
 	
 	m = length(y);%number of training examples
 	
 	J = 0;% we initialize the cost function to 0
 	
-	H = hypothesis(theta,x);
+	H = X*theta;
 	S = sum((H -y) .^ 2);
 	J = S/(2 *m);
 	
